68b3aaa8d27956182b1e0fb2f71547226dee7322
Longyarnz <longyarnz@gmail.com>
2017-12-07 05:09:17 +0100

    Add Resolvers

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,70 +1,82 @@
-// import Faker from 'faker';
-import UUID from 'uuid';
 export default {
   Query: {
     palettes: ({ Palette }, { options, limit, sort }) => Palette.get(options, limit, sort, Palette.disconnect),
     customers: ({ Customer }, { options, limit, sort }) => Customer.get(options, limit, sort, Customer.disconnect),
     audience: ({ Audience }, { options, limit, sort }) => Audience.get(options, limit, sort, Audience.disconnect),
-    platforms: ({ PLatform }, { options, limit, sort }) => PLatform.get(options, limit, sort, PLatform.disconnect)
+    platforms: ({ Platform }, { options, limit, sort }) => Platform.get(options, limit, sort, Platform.disconnect)
   },
   Mutation: {
-    CreatePalette: async ({ Palette }, { palette }) => {
-      const keyID = UUID.v4();
-      palette.keyID = keyID;
+    CreatePalette: ({ Palette }, { palette }) => {
       return Palette.set(palette, Palette.disconnect)
+    },
+    UpdatePalette: ({ Palette }, { palette }) => {
+      const { keyID } = palette;
+      return Palette.reset({ keyID }, palette).then(async i => {
+        if (i.nModified === 0) return null;
+        palette = await Palette.find({ keyID }, Palette.disconnect);
+        return palette[0];
+      }); 
+    },
+    CreatePlatform: ({ Platform }, { platform }) => {
+      return Platform.set(platform, Platform.disconnect)
+    },
+    UpdatePlatform: ({ Platform }, { platform }) => {
+      const { keyID } = platform;
+      return Platform.reset({ keyID }, platform).then(async i => {
+        if (i.nModified === 0) return null;
+        platform = await Platform.find({ keyID }, Platform.disconnect);
+        return platform[0];
+      });
+    },
+    RegisterCustomer: ({ Customer }, { customer }) => {
+      return (Customer.set(customer, Customer.disconnect))[0]
+    },
+    UpdateCustomer: ({ Customer }, { customer }) => {
+      const { keyID } = customer;
+      return Customer.reset({ keyID }, customer).then(async i => {
+        if (i.nModified === 0) return null;
+        customer = await Customer.find({ keyID }, Customer.disconnect);
+        return customer[0];
+      });
+    },
+    RegisterAudience: ({ Audience }, { audience }) => {
+      return Audience.set(audience, Audience.disconnect)
+    },
+    UpdateAudience: ({ Audience }, { audience }) => {
+      const { keyID } = audience;
+      return Audience.reset({ keyID }, audience).then(async i => {
+        if (i.nModified === 0) return null;
+        audience = await Audience.find({ keyID }, Audience.disconnect);
+        return audience[0];
+      });
+    },
+    RemoveEntries: async ({ Palette, Customer, Audience, Platform }, order) => {
+      let model, options;
+      for (const fields in order){
+        options = order[fields];
+        switch (fields) {
+        case 'palette':
+          model = Palette;  
+          break;
+        case 'customer':
+          model = Customer;  
+          break;
+        case 'audience':
+          model = Audience;  
+          break;        
+        default:
+          model = Platform;
+          break;
+        }    
+        options = await model.erase(options, model.disconnect);
+        options = options.result.n;
+      }    
+      return options > 0;
     }
+  },
+  Palette: obj => obj,
+  author: ({ author }, a, b, info) => {
+    console.log([ author, info ]);
+
   }
-// 		MakeRegularOrder: async ({ Invoice, User }, { client, invoice }) => {
-// 	    let check, invoice_number;
-// 	    do{
-// 	      invoice_number = Faker.random.number({min: 100000, max: 199999});
-// 	      check = await Invoice.numerate({ invoice_number });
-// 	      client.invoice = invoice.invoice_number = invoice_number;
-// 	    }
-// 	    while(check > 0);
-// 	    return User.set(client, User.disconnect).then(({ keyID }) => {
-// 	      invoice.user = keyID;
-// 	      return Invoice.set(invoice, Invoice.disconnect)
-// 	      .then(async () => {
-// 	      	let invoice = await Invoice.get({}, 1, '-createdAt', Invoice.disconnect, 'user');
-// 	      	invoice = invoice[0];
-// 	      	return { client, invoice, keyID }
-// 	      });
-// 	    });
-// 	  },
-// 		CreateItems: async ({ Item }, { items }) => {
-// 	    let keyID = await Item.get({}, 1, '-keyID', Item.disconnect);
-// 	    keyID = keyID[0].keyID;
-// 	    items.forEach(item => { item.keyID = ++keyID });
-// 	    return Item.set(items, Item.disconnect);
-// 	  },
-// 		UpdateItems: ({ Item }, { items }) => {
-// 	    const { keyID } = items;
-// 	    return Item.reset({ keyID }, items).then(async i => {
-// 	    	if (i.nModified === 0) return [];
-// 	      items = await Item.find({ keyID }, Item.disconnect);
-// 	      return items;
-// 	    }); 
-// 	  },
-// 		RemoveEntries: async ({ Item, Invoice, Client, User }, order) => {
-// 	    let model, options, all, response = {};
-// 	    for (const fields in order){
-// 	      all = false;
-// 	      options = order[fields];
-// 	      switch(fields){
-// 	        case 'allUsers': all = true; fields = 'user';
-// 	        case 'user': model = User; break;
-// 	        case 'allInvoices': all = true; fields = 'invoice';
-// 	        case 'invoice': model = Invoice; break;
-// 	        case 'allClients': all = true; fields = 'client';
-// 	        case 'client': model = Client; break;
-// 	        case 'allItems': all = true; fields = 'item';
-// 	        default: model = Item;
-// 	      }      
-// 	      options = all ? await model.erase({}, model.disconnect) : await model.erase({$or: options}, model.disconnect);
-// 	      response[fields] = options.result.n;
-// 	    }    
-// 	    return response;
-// 	  }
-// 	}
 }
