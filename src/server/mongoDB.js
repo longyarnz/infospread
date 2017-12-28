7f364f8894507703798dee77a963e3f3e04d8227
Longyarnz <longyarnz@gmail.com>
2017-12-28 05:24:00 +0100

    Change HOST and PORT values

--- src/server/mongoDB.js
+++ src/server/mongoDB.js
@@ -2,7 +2,7 @@ import Mongoose from 'mongoose';
 import assert from 'assert';
 import promise from 'bluebird';
 export default callback => {
-  const uriDB = 'mongodb://PowerUser:addingValue@infospread-shard-00-00-inwmw.mongodb.net:27017,infospread-shard-00-01-inwmw.mongodb.net:27017,infospread-shard-00-02-inwmw.mongodb.net:27017/infospread?ssl=true&replicaSet=Infospread-shard-0&authSource=admin';
+  const uriDB = 'mongodb://info:voissapp@127.0.0.1:27017/info_spread';
   Mongoose.connect(uriDB, { useMongoClient: true }, function(err){
     assert.equal(err, null);
     console.log('Database Connected...');
